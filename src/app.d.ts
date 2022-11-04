e815a4d184c58ea0a59cad527b0df4b90d54c472
devyuji <raghavancool24@gmail.com>
2022-11-04 13:04:52 +0530

    added name type for cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,8 +2,17 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+  // interface Locals {}
+  // interface PageData {}
+  // interface Error {}
+  // interface Platform {}
+  interface Platform {
+    env: {
+      COUNTER: DurableObjectNamespace;
+    };
+    context: {
+      waitUntil(promise: Promise<unknown>): void;
+    };
+    caches: CacheStorage & { default: Cache };
+  }
 }
